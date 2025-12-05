8f1cfbd7b742944a0332a3808aea60aa5aba45ea
Philipp Wilke <philippwilke.1996@web.de>
2025-12-05 13:06:03 +0900

    * rename npm package * fix gradle build for proper pushing

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -9,12 +9,16 @@ group = "com.meiji.build"
 version = "1.0.0"
 
 gradlePlugin {
+    website.set("https://github.com/meiji-academy/build-tools")
+    vcsUrl.set("https://github.com/meiji-academy/build-tools")
+
     plugins {
         create("environmentCheck") {
             id = "com.meiji.build.environment-check"
             implementationClass = "com.meiji.build.EnvironmentCheckPlugin"
             displayName = "Meiji Environment Check"
             description = "Enforces CI/CD and local environment configuration standards."
+            tags.set(listOf("build", "environment", "check", "ci", "security"))
         }
     }
 }
